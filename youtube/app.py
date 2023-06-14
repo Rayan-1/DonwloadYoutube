9f06dd41d07372555f59a887dc752a1a4da019b5
Rayan Victor <69490855+Rayan-1@users.noreply.github.com>
2023-06-14 20:29:07 -0300

    Update and rename youtube.py to app.py

---
+++ youtube/app.py
@@ -0,0 +1,36 @@
+import os
+from tkinter import Tk, Label, Entry, Button, Frame, font
+from pytube import YouTube
+from flask import Flask, render_template, request
+
+os.environ["DISPLAY"] = ":0.0"
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        link = request.form["link"]
+        path = request.form["path"]
+
+        try:
+            yt = YouTube(link)
+            title = "Título: " + yt.title
+            views = "Número de views: " + str(yt.views)
+            length = "Tamanho do vídeo: " + str(yt.length) + " segundos"
+            rating = "Avaliação do vídeo: " + str(yt.rating)
+
+            ys = yt.streams.get_highest_resolution()
+            download_label = "Baixando..."
+            ys.download(path)
+            download_label = "Download completo!"
+
+            return render_template("index.html", title=title, views=views, length=length, rating=rating, download_label=download_label)
+        except Exception as e:
+            error_message = "Erro ao baixar o vídeo"
+            return render_template("index.html", error_message=error_message)
+
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
